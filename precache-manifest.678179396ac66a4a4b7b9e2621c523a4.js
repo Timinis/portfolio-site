24c681d1d549d7b8d40b799ef46412378d4e16c7
Timinis <litimc3@gmail.com>
2019-03-29 05:23:09 -0700

    Updates

---
+++ precache-manifest.678179396ac66a4a4b7b9e2621c523a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "927eadc0733e213b8222",
+    "url": "/protfoli-site/static/js/runtime~main.fea727a6.js"
+  },
+  {
+    "revision": "c47db40a2c740c986e40",
+    "url": "/protfoli-site/static/js/main.b52719e0.chunk.js"
+  },
+  {
+    "revision": "f763e9a6662f085f2956",
+    "url": "/protfoli-site/static/js/2.489a9da2.chunk.js"
+  },
+  {
+    "revision": "c47db40a2c740c986e40",
+    "url": "/protfoli-site/static/css/main.e59c6cc7.chunk.css"
+  },
+  {
+    "revision": "37f6a8ea58a1ae93bb63b8686c375c30",
+    "url": "/protfoli-site/index.html"
+  }
+];
